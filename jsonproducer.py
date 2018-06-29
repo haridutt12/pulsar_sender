dfecc8c5db205b1c91d80b43e2c92c2faa63b7b7
Hari <ashariduttparashar@gmail.com>
2018-06-29 14:10:28 +0530

    streaming integer sender

--- jsonproducer.py
+++ jsonproducer.py
@@ -5,12 +5,15 @@ client = pulsar.Client('pulsar://localhost:6650')
 data = {}
 
 producer = client.create_producer('persistent://sample/standalone/ns1/my-topic')
-
-for i in range(100):
+i=0
+while True:
     try:
-        data['i'] = 'hello-Pulsar'
-        json_data = json.dumps(data)
-        producer.send(json_data)
+#        data['i'] = i+1
+#        json_data = json.dumps(data)
+        i=i+1
+        producer.send("%d" %i)
+        if i == 1000:
+            break
     except Exception as e:
         print("Failed to send message: %s", e)
 
