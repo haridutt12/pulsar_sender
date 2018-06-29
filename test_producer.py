56580c69fcf4b872536d2e94ec548e1d5210e109
Hari <ashariduttparashar@gmail.com>
2018-06-29 14:09:21 +0530

    csv file sender

--- test_producer.py
+++ test_producer.py
@@ -1,13 +1,19 @@
 import pulsar
+import csv
+f = open("Iris.csv", "rb") 
+reader = csv.reader(f)
 
 client = pulsar.Client('pulsar://localhost:6650')
 
 producer = client.create_producer('persistent://sample/standalone/ns1/my-topic')
 
-for i in range(10):
-    try: 
-        producer.send('hello-Pulsar-%d' % i)
-    except Exception as e:
-        print("Failed to send message: %s", e)
+# for i in range(10):
+#     try: 
+#         producer.send('hello-Pulsar-%d' % i)
+#     except Exception as e:
+#         print("Failed to send message: %s", e)
+for row in reader:
+    d = json.dumps(row)
+    producer.send(d)
 
 producer.close()
